9ce96622b94934db4b7a05cbe21ed91afa03e90e
Antony rubombora <tonyrubombora@gmail.com>
2022-01-13 01:25:37 +0300

    fixed startup error of unrecognized window DOM

--- startup.js
+++ startup.js
@@ -1,10 +1,12 @@
 function start() {
 
-    //open new tab as soon as browser is opened
-    const url = "https://www.moodle.tum.de/login/index.php";
-    window.open(url, '_blank').focus();
-    console.log("Moodle openning");
-
+    if (typeof window !== "undefined") {
+        // browser code    
+        //open new tab as soon as browser is opened
+        const url = "https://www.moodle.tum.de/login/index.php";
+        window.open(url, '_blank').focus();
+        console.log("Moodle openning");
+    }
 }
 
 start()
