70ca84aa38a82516b752eeadce9076d8182690fb
costa-jonathan <jonathanleonhard.costa@gmail.com>
2022-03-12 15:05:35 +0100

    added an english disclaimer in the options

---
+++ srcOptions/disclaimer_en.js
@@ -0,0 +1,22 @@
+document.write('\
+<p>\
+Your login data is stored <b>locally</b> and <b>unencrypted</b> somewhere in the files \
+for this Chrome extension. This means anybody with access to this computer can potentially \
+access your password, even if that access is a remote one (Team Viewer or potential spyware). \
+It is not synchronized so it stays localized which means that it cannot be scooped \
+by somebody watching your network traffic or something similar. \
+<br>\
+Why do we compare the security to that of the chrome/edge password autofill feature? \
+Because if someone would go to the login page of moodle and your passwords gets autofilled, \
+it is a matter of a few clicks to copy the password out of the password window \
+(change "type" of the field in the html code). \
+<br>\
+Personally, I (Jonathan) am using my own extension (I initially only wrote it for myself), so I think it \
+is probably fine to use, but I am not studying information security or anything close to that so I \
+might be wrong. \
+<br>\
+You can use this extension even without providing your TUM login. It will still save you one or \
+two click here and there - that is the whole point after all. \
+</p>\
+\
+')
