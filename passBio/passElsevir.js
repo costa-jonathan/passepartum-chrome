29b6b40548e5025ba56dbfd1adcacf7c5e44d200
Jonathan <jonathanleonhard.costa@gmail.com>
2022-04-21 18:31:45 +0200

    biotech branch opened

---
+++ passBio/passElsevir.js
@@ -0,0 +1,1 @@
+document.getElementById('bdd-elsSecondaryBtn').click()
---
+++ passBio/passReaxys.js
@@ -0,0 +1,18 @@
+// window.addEventListener('load', function () {
+try {
+    document.getElementsByClassName('new-menu-link')[1].click()
+} catch (e) {
+    window.location.href = '\#/institution'; //'https://www.reaxys.com/
+}
+// window.location.href = '\#/institution'; //'https://www.reaxys.com/
+window.addEventListener('load', function () {
+    // probably forbidden: document.getElementsByClassName('login-page-form-caption typography-small')[0].innerHTML = 'Sign in via TUM for the first time to enable the automation by Passepartum'
+})
+
+
+//window.addEventListener('load', function () {
+//    document.getElementById("search-institutions-typeahead").value = "TUM"
+//    document.querySelector('[title="Technical University of Munich (TUM)"]').click()
+//
+
+// todo: https://www.science.org/
