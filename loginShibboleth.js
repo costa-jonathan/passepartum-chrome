7af2512819709b86f26b063b3062350871dd1d36
Jonathan <jonathanleonhard.costa@gmail.com>
2022-04-23 09:35:33 +0200

    latex.tum.de automation implemented

--- loginShibboleth.js
+++ loginShibboleth.js
@@ -4,17 +4,18 @@ window.addEventListener('load', function() {
     document.getElementById("donotcache-dummy-label").click() // click the "remember me" radio
 
     // first get stored username and password - if that fails run main which relies on chrome saved passwords
-    try { // this try catch block doesn't work yet todo
+    try {
         chrome.storage.sync.get('moodle_key', function(items) {
             if (!chrome.runtime.error && document.getElementsByClassName('form-error').length === 0) {
                 document.getElementById('username').value = items.moodle_key.username
                 document.getElementById('password').value = items.moodle_key.password
+                // wait a bit and then click the login button
+                setTimeout(() => { document.getElementById("btnLogin").click() }, 500);
             }
         });
     } catch (err) {
         main().then() // async function but don't need then I guess
     }
 
-    // wait a bit and then click the login button
-    setTimeout(() => { document.getElementById("btnLogin").click() }, 1000);
+
 })
---
+++ passTUMLaTeX.js
@@ -0,0 +1,29 @@
+
+
+window.addEventListener('load', function() {
+    // first get stored username and password - if that fails run main which relies on chrome saved passwords
+    try {
+        chrome.storage.local.get('moodle_key', function(items) {
+            if (!chrome.runtime.error && document.getElementsByClassName('form-error').length === 0) {
+                document.getElementsByName('login')[0].value = items.moodle_key.username
+                document.getElementsByName('password')[0].value = items.moodle_key.password
+                document.getElementsByClassName("btn-primary btn")[0].click()
+            }
+        });
+    } catch (err) {
+        main().then() // async function but don't need then I guess
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
