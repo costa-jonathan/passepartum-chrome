ea9b72f325c73accd0cc068d08b8942691d7e659
Antony rubombora <tonyrubombora@gmail.com>
2022-05-08 18:05:10 +0200

    spellcheck

--- srcPopup/delete.js
+++ srcPopup/delete.js
@@ -1,16 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.delete').addEventListener('click', onclick, false)
-    function onclick () {
+
+    function onclick() {
         let username = ''
         let password = ''
         console.log(username, password)
-        chrome.storage.local.set({'moodle_key' : {"username": username, "password": password}}, function(){
-            if(chrome.runtime.error){
-                console.error("An error with local storage was found. If this precists please contact us.");
+        chrome.storage.local.set({ 'moodle_key': { "username": username, "password": password } }, function() {
+            if (chrome.runtime.error) {
+                console.error("An error with local storage was found. If this presists please contact us.");
             }
         });
         location.reload() // reloading so one can see a successful input
     }
 })
-
-
--- srcPopup/save.js
+++ srcPopup/save.js
@@ -1,28 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.local.get('moodle_key', function(items){
-    if(!chrome.runtime.error){
-        let user = items.moodle_key.username
-        if (user) {
-            document.getElementById('title').innerHTML =
-                'There is login information stored for <b>' + user + '</b>. ' +
-                'If you want to change it simply enter the new login data.' +
-                ''
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.storage.local.get('moodle_key', function(items) {
+        if (!chrome.runtime.error) {
+            let user = items.moodle_key.username
+            if (user) {
+                document.getElementById('title').innerHTML =
+                    'There is login information stored for <b>' + user + '</b>. ' +
+                    'If you want to change it simply enter the new login data.' +
+                    ''
+            }
         }
-    }
     })
 
     document.querySelector('.save').addEventListener('click', onclick, false)
-    function onclick () {
+
+    function onclick() {
         let username = document.getElementById('username').value
         let password = document.getElementById('password').value
         console.log(username, password)
-        chrome.storage.local.set({'moodle_key' : {"username": username, "password": password}}, function(){
-            if(chrome.runtime.error){
-                console.error("An error with local storage was found. If this precists please contact us.");
+        chrome.storage.local.set({ 'moodle_key': { "username": username, "password": password } }, function() {
+            if (chrome.runtime.error) {
+                console.error("An error with local storage was found. If this presists please contact us.");
             }
         });
         location.reload() // reloading so you can see a successful input
     }
 })
-
-
