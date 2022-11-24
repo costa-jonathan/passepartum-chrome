983700589b9b9656adfc4833fccfe184facd0aa7
Antony rubombora <tonyrubombora@gmail.com>
2022-11-24 18:09:37 +0300

    new feedback link discoverable on passepartum@...

--- srcPopup/feedback.js
+++ srcPopup/feedback.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.feedback').addEventListener('click', onclick, false)
-    function onclick () {
+
+    function onclick() {
         document.getElementById('bod').innerHTML = '' +
-            // '<h1>Please consider filling in the quick form below, alternatively </h1>' +
-        '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScMDrXN8wgb6IECqNw5Yrw0rx8cgUK_ZSNhuw--b1kTVznNfg' +
-            '/viewform?embedded=true" width="640" height="597">Loading…</iframe>'
+            '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe8gyaUI19rbe5wbrZtwNFUiYO3VcpebFK29huEK5V7L3rpCw/viewform?embedded=true" width="300" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>'
     }
 })
