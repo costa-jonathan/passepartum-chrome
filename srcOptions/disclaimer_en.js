dccd86857fa6595efa3352ec670e9bb009331a12
Antony rubombora <tonyrubombora@gmail.com>
2023-03-26 20:05:03 +0200

    disclaimer & licence edits

--- srcOptions/disclaimer_en.js
+++ srcOptions/disclaimer_en.js
@@ -3,33 +3,34 @@ document.write('\
 <h1>Disclaimer</h1>\
 <div class="text"> \
 <b>Storage</b>\
-<br>\
-Your login data is stored <b>locally</b> and <b><a href="https://developer.chrome.com/docs/extensions/reference/storage/" target="_blank">unencrypted</a></b> somewhere in the files \
-for this Chrome extension. This means anybody with access to this computers local storage can potentially \
-access your password, even if that access is a remote one (like Team Viewer or potential spyware). \
-It is not synchronized via cloud storage, which means it cannot be scooped \
-by somebody watching your network traffic. \
-<br>\
-<br>\
-<b>Why do we compare the security to that of the chrome/edge password autofill feature?</b>\
-<br>\
-Because if someone would go to the login page of moodle and your passwords gets autofilled, \
-it is a matter of a few clicks to copy the password out of the password window. \
-<br>\
-<br>\
-<b>The devs</b>\
-<br>\
-Non of the people writing this extension have a background in information security, so we cannot \
-promise a flawless experience. However all of the developers use this extension and trust it\'s basic functionality.\
-<br>\
-<br>\
-<b>What if you don\'t provide the login data?</b>\
-<br>\
-You can use this extension even without providing your TUM login. It will still save you one or \
-two click here and there - that is the whole point after all. \
-\
-<br>\
-<br>\
+<section>\
+<h2>Data Security</h2>\
+<p>The password-keeping Chrome extension uses SHA256 encryption technology to secure your passwords. However, there is always a risk of data breaches, hacking, or other unauthorized access. The developer of this extension cannot guarantee the security of your data.</p>\
+</section>\
+<section>\
+<h2>User Responsibility</h2>\
+<p>You are responsible for safeguarding your passwords and maintaining the confidentiality of your login credentials. The developer of this extension is not responsible for any loss or damage resulting from your failure to protect your passwords.</p>\
+</section>\
+<section>\
+<h2>Third-Party Services</h2>\
+<p>The password-keeping Chrome extension does not use third-party services for data storage or other functions. The developer of this extension is not responsible for the privacy practices third-party services.</p>\
+</section>\
+<section>\
+<h2>Use at Your Own Risk</h2>\
+<p>The password-keeping Chrome extension is designed to help you manage your passwords. However, no password management system is completely secure. You use the password-keeping Chrome extension at your own risk. The developer of this extension assumes no liability for any loss or damage caused by the use of this extension.</p>\
+</section>\
+<section>\
+<h2>Updates</h2>\
+<p>The password-keeping Chrome extension may be updated from time to time to improve functionality or security. It is your responsibility to keep the extension up-to-date.</p>\
+</section>\
+<section>\
+<h2>Use of Information</h2>\
+<p>The password-keeping Chrome extension does not collect information about your use of the extension. The developer of this extension will only use private information for security functionality of the extension.</p>\
+</section>\
+<section>\
+<h2>Modifications</h2>\
+<p>The developer of this extension reserves the right to modify this disclaimer at any time. You are responsible for reviewing this disclaimer periodically to ensure that you are aware of any changes.</p>\
+</section>\
 <div class="small-text">\
 <a target="_blank" href="https://icons8.com/icon/113135/schlüssel">Schlüssel</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>\
 </div>\
