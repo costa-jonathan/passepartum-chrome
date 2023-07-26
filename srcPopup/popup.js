8f971f57216336a21ef5506e36567eb0efe2ecc8
Magala Reuben <reuben.magala@piclepay.com>
2023-07-26 10:22:25 +0300

    Added pacman game into the extension

--- srcPopup/popup.js
+++ srcPopup/popup.js
@@ -1,62 +1,62 @@
- function createTranslationMap(original, translation) {
-    console.log(original);
-    console.log(translation);
-    const translationMap = {};
+//  function createTranslationMap(original, translation) {
+//     console.log(original);
+//     console.log(translation);
+//     const translationMap = {};
 
-  // Check if both lists have the same length
-  if (original.length !== translation.length) {
-    console.error("The 'original' and 'translation' lists should have the same length.");
-    //return null;
-  }
+//   // Check if both lists have the same length
+//   if (original.length !== translation.length) {
+//     console.error("The 'original' and 'translation' lists should have the same length.");
+//     //return null;
+//   }
 
-  // Create the translation mapping
-  for (let i = 0; i < original.length; i++) {
-    const originalText = original[i];
-    const translatedText = translation[i];
-    translationMap[originalText] = translatedText;
-  }
+//   // Create the translation mapping
+//   for (let i = 0; i < original.length; i++) {
+//     const originalText = original[i];
+//     const translatedText = translation[i];
+//     translationMap[originalText] = translatedText;
+//   }
 
-  return translationMap;
-}
+//   return translationMap;
+// }
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const browserLanguage = navigator.language;
-    console.log(navigator.language)
-    const allText = document.body.innerText;
-    const textLines = allText.split('\n').map(line => line.trim()).filter(line => line !== '');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const browserLanguage = navigator.language;
+//     console.log(navigator.language)
+//     const allText = document.body.innerText;
+//     const textLines = allText.split('\n').map(line => line.trim()).filter(line => line !== '');
 
-    const url = ' http://127.0.0.1:8000/api/v1/translate/';
-    const data = {
-    language: browserLanguage,
-    text: textLines,
-    };
-    fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-    })
-   .then(response => response.json())
-   .then(data =>  {
+//     const url = ' http://127.0.0.1:8000/api/v1/translate/';
+//     const data = {
+//     language: browserLanguage,
+//     text: textLines,
+//     };
+//     fetch(url, {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//     })
+//    .then(response => response.json())
+//    .then(data =>  {
     
-    const translationMap = createTranslationMap(data['original'], data['translation']);
-    console.log(translationMap);
-    if (translationMap && browserLanguage == "de") {
-        const elements = document.body.getElementsByTagName("*");
-        for (let i = 0; i < elements.length; i++) {
-            const element = elements[i];
-            const textContent = element.textContent.trim();
+//     const translationMap = createTranslationMap(data['original'], data['translation']);
+//     console.log(translationMap);
+//     if (translationMap && browserLanguage == "de") {
+//         const elements = document.body.getElementsByTagName("*");
+//         for (let i = 0; i < elements.length; i++) {
+//             const element = elements[i];
+//             const textContent = element.textContent.trim();
 
-            if (translationMap.hasOwnProperty(textContent)) {
-            element.textContent = translationMap[textContent];
-            }
-        }
-}
-});
-});
+//             if (translationMap.hasOwnProperty(textContent)) {
+//             element.textContent = translationMap[textContent];
+//             }
+//         }
+// }
+// });
+// });
 
 
 
