fa62fd8557b41bb9bb03ab06e88c39df15a40005
costa <jonathanleonhard.costa@gmail.com>
2023-08-24 14:39:49 +0200

    refactor:rephrasing disclaimer (en)

--- srcOptions/disclaimer_en.js
+++ srcOptions/disclaimer_en.js
@@ -4,30 +4,28 @@ document.write('\
 <div class="text"> \
 <b>Storage</b>\
 <br>\
-Your login data is stored <b>locally</b> and <b><a href="https://developer.chrome.com/docs/extensions/reference/storage/" target="_blank">unencrypted</a></b> somewhere in the files \
-for this Chrome extension. This means anybody with access to this computers local storage can potentially \
+Your login data is stored <b>locally</b> and <b>unencrypted</b> (using <a href="https://developer.chrome.com/docs/extensions/reference/storage/" target="_blank">this feature</a>).\
+This means anybody with access to your computers local storage can theoretically (if they know where to look) \
 access your password, even if that access is a remote one (like Team Viewer or potential spyware). \
-It is not synchronized via cloud storage, which means it cannot be scooped \
-by somebody watching your network traffic. \
+It is not synchronized via cloud storage, which means it <b>cannot</b> be leaked by an external data breach or \
+by somebody monitoring your network traffic. \
 <br>\
 <br>\
 <b>Why do we compare the security to that of the chrome/edge password autofill feature?</b>\
 <br>\
-Because if someone would go to the login page of moodle and your passwords gets autofilled, \
-it is a matter of a few clicks to copy the password out of the password window. \
+Because if someone with access to your computer would go to the login page of moodle and your passwords gets autofilled, \
+it is a matter of a few clicks to copy the password out of the password field. \
 <br>\
 <br>\
-<b>The devs</b>\
+<b>The Developers</b>\
 <br>\
-Non of the people writing this extension have a background in information security, so we cannot \
-promise a flawless experience. However all of the developers use this extension and trust it\'s basic functionality.\
+There is no person with a background in information security working on this extension.\
 <br>\
 <br>\
 <b>What if you don\'t provide the login data?</b>\
 <br>\
-You can use this extension even without providing your TUM login. It will still save you one or \
-two click here and there - that is the whole point after all. \
-\
+You can still use this extension even without providing your TUM login. It will still save you one or \
+two click here and there. \
 <br>\
 <br>\
 <div class="small-text">\
